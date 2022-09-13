81781a86cff3bc1f0fba87dee60de1ca65513c33
0049003919 <0049003919@znv.com>
2022-09-13 16:00:24 +0800

    优化日志

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -13,15 +13,14 @@ public class EurekaApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
-
 //	public static void main(String[] args) {
-//		SpringApplication app = new SpringApplication(EurekaApplication.class);
-//		Environment env = app.run(args).getEnvironment();
-//		LOG.info("启动成功！！");
-//		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+//		SpringApplication.run(EurekaApplication.class, args);
 //	}
 
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
 }
